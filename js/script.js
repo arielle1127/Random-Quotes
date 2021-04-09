dc472aa8f667aed70fac1169a1482790ecbc287c
arielle1127 <81972975+arielle1127@users.noreply.github.com>
2021-04-09 09:28:31 +0400

    Code comments

--- js/script.js
+++ js/script.js
@@ -3,13 +3,18 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-const main = document.querySelector('main');
 
-const quotes = [
+
+const main = document.querySelector('main');
+let quoteBox = document.getElementById('quote-box'); //returns complete HTML string of quotes, source, citations, etc
+
+
+const quotes = [       //array of quotes to be printed to page
   {
     quote: 'If your compassion does not include yourself, it is incomplete.',
     source: 'Jack Kornfield',
-    citation: 'Buddha’s Little Instruction Book'
+    citation: 'Buddha’s Little Instruction Book',
+    year: 1994
   },
   {
     quote: 'Being at ease with not knowing is crucial for answers to come to you.',
@@ -21,17 +26,30 @@ const quotes = [
   },
   {
     quote: 'A single event can awaken within us a stranger totally unknown to us. To live is to be slowly born.',
-    source:'Antoine de Saint-Exupery'
+    source:'Antoine de Saint-Exupery',
+    bio: 'French aviator and author',
+    year: '1900-1944'
+  },
+  {
+    quote: 'Emotions are like waves. Watch them disappear in the distance on the vast calm ocean.',
+    source:'Ram Dass',
+    citation: 'Be Here Now',
+    year: 1971
   },
   {
     quote: 'Your task is not to seek for love, but merely to seek and find all the barriers within yourself that you have built against it.',
     source:'Rumi',
-    year: 1207
+    year: '1207-1241'
   } 
 ]; 
 
 console.log(quotes);
 
+
+/*   getRandomQuote() function generates random quote from array 
+     randomNumber generates random index from array
+     randomQuote returns quote, source, citation, etc from random index
+*/
 
 function getRandomQuote() {
     let randomNumber = Math.floor(Math.random() * quotes.length);
@@ -41,58 +59,29 @@ function getRandomQuote() {
 }
 getRandomQuote();
 
-//I can only get the html to work when i put the document.getelementByID inside the function.
-//It wasn't working outside the function because the html variable was in the scope of the function
-//Is this right or is there a better way?
+/*  printQuote() function prints randomQuote to page
+    randomquoteObj calls getRandomQuote() function to generate a random quote
+    First two lines of html prints quote and source of randomquoteObj 
+    If randomquoteObj contains a citation, bio, or year it prints that 
+    quoteBox.innerHTML calls quoteBox variable to print entire HTML string
+*/ 
 
 function printQuote() {
     let randomquoteObj = getRandomQuote();
     let html = `<p class="quote">${randomquoteObj.quote}</p>
-                <p class="source">${randomquoteObj.source}<p>`;
-      document.getElementById('quote-box').innerHTML = html;
+                <p class="source">${randomquoteObj.source}`;
+      if ('citation' in randomquoteObj) {
+          html += `<span class="citation">${randomquoteObj.citation}</span>`;
+             }
+      if ('bio' in randomquoteObj) {
+            html += `, <span class="bio">${randomquoteObj.bio}</span>`;
+               }
+      if ('year' in randomquoteObj) {
+              html += `<span class="year">${randomquoteObj.year}</span> </p>`;
+           }           
+     quoteBox.innerHTML = html;
 }
 printQuote();
-
-
-
-
- 
-/* 
-
-Create two separate if statements below the variables. You will need to add decision making to this function:
-Use an if statement to check if the citation property exists, and if it does, concatenate a <span></span> element, appropriate className, and citation property to the HTML string
-* If the random quote object has a citation property, concatenate a <span> element with the class "citation" to the HTML string.
-
- Use an if statement to check of the year property exists, and if it does, concatenate a <span></span> element, appropriate className, and year property to the HTML 
- If the random quote object has a year property, concatenate a <span> element with the class "year" to the HTML string.
- 
- After the two if statements, concatenate the closing </p> tag to the HTML string
-set the innerHTML of the quote-box div to equal the complete HTML string*/ 
-
-
-if () {
-
-};
-
-
- 
-
-/*Use the following code snippet as a guide for what the HTML string should look like with the added "citation" and "year" <span> elements 
-(like earlier, omit the second closing </p> tag for now):
-
-
-<p class="quote"> A random quote </p>
-<p class="source"> quote source
-  <span class="citation"> quote citation </span>
-  <span class="year"> quote year </span>
-</p>
-
-Below the if statements, complete the string by concatenating a closing </p> tag to the HTML string.
- This is the closing tag for the second paragraph with the class source.
-
-Lastly, set the printQuote function to return the full HTML string displaying a random quote.
-
-
 
 
 /* 
